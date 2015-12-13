5aa9986c58743f48dd99446a508e35d51e47712d
Geoyeob Kim <gasbank@msn.com>
2015-12-13 18:58:14 +0900

    첫 커밋

---
+++ Assets/BoardPhysics.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardPhysics : MonoBehaviour
+{
+    public GameObject coinGroup;
+    public Transform forceApplyBeginPoint;
+    public Transform forceApplyEndPoint;
+    public float forceYMagnitude = 1.0f;
+    public float forceScaler = 10.0f;
+
+    public void ApplyRandomForce()
+    {
+        var force = (forceApplyEndPoint.position - forceApplyBeginPoint.position).normalized;
+        force += Vector3.up * forceYMagnitude;
+        force *= forceScaler;
+
+        Debug.Log("Apply force: " + force);
+        for (int i = 0; i < coinGroup.transform.childCount; i++)
+        {
+            var c = coinGroup.transform.GetChild(i);
+            var rb = c.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                var dist = (c.transform.position - forceApplyBeginPoint.position).magnitude;
+                
+                rb.AddForceAtPosition(force / dist, forceApplyBeginPoint.position);
+            }
+            
+        }
+    }
+}
