9c69900d72c2d3829c7da97a346bbe73786cb645
Geoyeob Kim <gasbank@gmail.com>
2019-11-08 11:49:32 +0900

    히익

--- Assets/CoinSmashers/Scripts/SpringJointConfig.cs
+++ Assets/CoinSmashers/Scripts/SpringJointConfig.cs
@@ -14,6 +14,11 @@ namespace Assets.CoinSmashers.Scripts
         [SerializeField]
         private float damper = 1.0f;
 
+        [SerializeField]
+        private float maxDistance = 2.0f;
+        [SerializeField]
+        private float minDistance = 1.0f;
+
         void OnValidate()
         {
             var multiplier = new[] { new[] { 1, 1 }, new[] { 1, -1 }, new[] { -1, 1 }, new[] { -1, -1 } };
@@ -23,6 +28,8 @@ namespace Assets.CoinSmashers.Scripts
                 springJointList[i].anchor = new Vector3(boxCollider.size.x / 2 * multiplier[i][0], -boxCollider.size.y / 2, boxCollider.size.z / 2 * multiplier[i][1]);
                 springJointList[i].spring = spring;
                 springJointList[i].damper = damper;
+                springJointList[i].maxDistance = maxDistance;
+                springJointList[i].minDistance = minDistance;
             }
         }
     }
